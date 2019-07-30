bd90a11651bcdd7926af1b785252f7066e04be11
Philippe Vinchon <p.vinchon@gmail.com>
2019-07-31 00:09:27 +0100

    Hyper hello-world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate hyper;
+
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Request, Response, Server};
+
+fn hello_world(_req: Request<Body>) -> Response<Body> {
+    Response::new(Body::from("Hello, world!"))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let new_svc = || service_fn_ok(hello_world);
+
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Listening on http://{}", addr);
+
+    hyper::rt::run(server);
 }
